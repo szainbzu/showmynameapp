cc0446f7ef1251a37595197361ae7b0a51d4e95e
szainbzu <szain@birzeit.edu>
2021-03-13 13:04:02 +0200

    Version 1.0

---
+++ app/src/main/java/edu/cs/birzeit/showmynameapp/MainActivity.java
@@ -0,0 +1,26 @@
+package edu.cs.birzeit.showmynameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText edtName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtName = findViewById(R.id.edtName);
+    }
+
+    public void btnShow_onClick(View view) {
+        String name = edtName.getText().toString().trim();
+        if(!name.isEmpty()){
+            Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
